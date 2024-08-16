855e8315fc79459ed8854cb36b225373b51dc978
janisfs <janisfs@mail.ru>
2024-08-16 20:01:32 +0300

    add new  code

--- part2.py
+++ part2.py
@@ -3,15 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import csv
 
 # Настройка драйвера
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # Запуск в фоновом режиме (без графического интерфейса)
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+# options = webdriver.ChromeOptions()
+# options.add_argument('--headless')  # Запуск в фоновом режиме (без графического интерфейса)
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
 
 # Инициализация драйвера
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver = webdriver.Chrome()
 
 # Открытие страницы
 url = 'https://www.cian.ru/snyat-kvartiru-1-komn-ili-2-komn/'
@@ -21,12 +22,17 @@ driver.get(url)
 time.sleep(5)
 
 # Поиск элементов с ценами
-price_elements = driver.find_elements(By.XPATH, '//div[contains(@class, "c6e8ba9eec--price--2nM5Y")]')
+prices = driver.find_elements(By.XPATH, "//span[@data-mark='MainPrice']/span")
 
-# Извлечение и вывод цен
-prices = [price.text for price in price_elements]
-for price in prices:
-    print(price)
+# Сохранение цен в CSV файл
+csv_file = 'prices.csv'
+with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Price'])  # Заголовок
+    for price in prices:
+        writer.writerow([price.text])  # Запись текста цен
+
+print(f"Цены сохранены в файл {csv_file}")
 
 # Закрытие драйвера
 driver.quit()
